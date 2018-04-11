f1dc990f8180d80d31e3d0e53ebc96e27c425719
ving10785 <vincent.nguyen96@ymail.com>
2018-04-11 10:51:35 +0200

    Passage html->php
    rajout du header.php, etc...

---
+++ app_info/dashboard_compte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Virifocus </title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="dashboard_compte.css"/>
+    <link rel="icon" type="image/png" href="image/logo.png" />
+</head>
+<body class="fond">
+            <div id="site">
+			
+			<?php
+				include "header.php";
+			?>
+
+            </div>
+</body>
