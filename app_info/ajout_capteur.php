a13c3e1cc9770759ca03116074a007afa33c7d25
maxencelebec <maxence.lbc@gmail.com>
2018-05-01 23:46:20 +0200

    finalisation

--- app_info/ajout_capteur.php
+++ app_info/ajout_capteur.php
@@ -8,31 +8,53 @@ function ajout_capteur($capteur_actionneur)
 	if ($capteur_actionneur=="luminosité")
 	{
 		?>
-			<div class="capteur">Luminosité</div>
+			<div class="capteur">Luminosité <br/> 220 Lux </br>
+				<?php
+                    include 'boutton.php';
+                ?>
+			</div>
 		<?php
 	}
 	elseif ($capteur_actionneur=="temperature")
 	{
 		?>
-			<div class="capteur">Température</div>
+			<div class="capteur">Température <br/> 23°C </br>
+				<?php
+                    include 'boutton.php';
+                ?>
+			</div>
 		<?php
 	}
 	elseif ($capteur_actionneur=="presence")
 	{
 		?>
-			<div class="capteur">Présence</div>
+			<div class="capteur">Présence <br/> OUI </br>
+				<?php
+                    include 'boutton.php';
+                ?>
+			</div>
 		<?php
 	}
 	elseif ($capteur_actionneur=="humidité")
 	{
 		?>
-			<div class="capteur">Humidité</div>
+			<div class="capteur">Humidité <br/> 24% </br> 
+				<?php
+                    include 'boutton.php';
+                ?>
+			</div>
 		<?php
 	}
 	elseif ($capteur_actionneur=="porte")
 	{
 		?>
-			<div class="capteur">Porte</div>
+			<div class="capteur">Porte <br/>
+				<?php
+                    include 'boutton.php';
+                ?>
+			</br>
+			Ouvert/fermé
+			</div>
 		<?php
 	}
 }
