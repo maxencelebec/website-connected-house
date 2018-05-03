936861170ac8719e1ef7942c7e9eebaa1bc25f92
ving10785 <vincent.nguyen96@ymail.com>
2018-05-03 22:37:25 +0200

    PUUUSH

--- app_info/dashboard_compte.php
+++ app_info/dashboard_compte.php
@@ -6,47 +6,26 @@
     <link rel="stylesheet" href="dashboard_compte.css"/>
     <link rel="icon" type="image/png" href="image/logo.png" />
 	<?php
-
-// Name of the file
-$filename = 'users.sql';
-// MySQL host
-$mysql_host = 'localhost';
-// MySQL username
-$mysql_username = 'root';
-// MySQL password
-$mysql_password = '';
-// Database name
-$mysql_database = 'virifocus';
-
-// Connect to MySQL server
-mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
-// Select database
-mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
-
-// Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
-$lines = file($filename);
-// Loop through each line
-foreach ($lines as $line)
-{
-// Skip it if it's a comment
-if (substr($line, 0, 2) == '--' || $line == '')
-    continue;
-
-// Add this line to the current segment
-$templine .= $line;
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // Perform the query
-    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-    // Reset temp variable to empty
-    $templine = '';
-}
-}
- echo "Tables imported successfully";
-?>
+		try
+		{
+			$bdd = new PDO('mysql:host=localapp;dbname=virifocus;charset=utf8', 'root', '');
+		}
+		catch(Exception $e)
+		{
+				die('Erreur : '.$e->getMessage());
+				echo "ERREUR BDD ERREUR BDD ERREUR BDD";
+		}
+		/* Requêtes des différentes données du compte */
+		$request_name = $bdd->query('SELECT name FROM users WHERE mail="vincent.nguyen@96ymail.com"');
+		$request_firstname = $bdd->query('SELECT firstname FROM users WHERE mail="vincent.nguyen@96ymail.com"');
+		/* Attribution variable des données */
+		 while ($donnees = $request_name->fetch()) {
+			$name = $donnees['name'];
+		}
+		while ($donnees = $request_firstname->fetch()) {
+			$firstname = $donnees['firstname'];
+		}
+	?>
 </head>
 <body class="fond">
             <div id="site">
@@ -63,17 +42,17 @@ if (substr(trim($line), -1, 1) == ';')
 				<button class="tablinks" id="tablink4">Notifications</button>
 				</div>
 				
-					<!-- Tab 1 : Profile -->
-						<div id="profile" class="tabcontent">
-							<form> 
-								Name: <input type="text" name="name" value="<?php echo $name;?>">
-								Firstname: <input type="text" name="firstname" value="<?php echo $firstname;?>">
-							</form>
-							<hr />
-							<p> J'aimerai savoir comment faire une bonne pur&eacute;e. </p>
-							<hr />
-							<p> Quel est votre secret ? </p>
-						</div>			
+				<!-- Tab 1 : Profile -->
+				<div id="profile" class="tabcontent">
+					<form class="profile_form" action="dashboard_compte_submit.php" method="post"> 
+						Name: <input type="text" name="name" value="<?php echo $name;?>">
+						Firstname: <input type="text" name="firstname" value="<?php echo $firstname;?>">
+					</form>
+					<hr />
+					<p> J'aimerai savoir comment faire une bonne pur&eacute;e. </p>
+					<hr />
+					<p> Quel est votre secret ? </p>			
+				</div>			
 						
 					<!-- Tab 2 : Compte -->
 						<div id="compte" class="tabcontent"> Test2 </div>		
@@ -83,12 +62,13 @@ if (substr(trim($line), -1, 1) == ';')
 						
 					<!-- Tab 4 : Notifications -->
 						<div id="notifications" class="tabcontent"> Test4 </div>
-			
+						
+				<input class="valider" type="submit" value="Valider">
 			<?php
 				include "footer.php";
 			?>
 			
-            </div>
+			</div>
 		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 		<script src="dashboard_compte.js"></script>
 </body>
