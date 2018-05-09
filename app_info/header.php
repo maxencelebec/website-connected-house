4c9fbdca68e680b843aed5f885a894dfa351ef56
ThierDev <thierry-lincoln@hotmail.com>
2018-05-09 10:43:16 +0200

    Scaling_Header_fixed

--- app_info/header.php
+++ app_info/header.php
@@ -3,16 +3,16 @@
     <div class="header">
         <div class="home">
              <a class="lien" href="dashboard_simple.php" >
-                <div class="home_lien"><br><br><br>Home</div></a></div>
+                <div class="home_lien"><br><br>Home</div></a></div>
         <div class="compte">
                <a class="lien" href="dashboard_compte.php">
-                 <div class="compte_lien"><br><br><br>Compte</div></a></div>
+                 <div class="compte_lien"><br><br>Compte</div></a></div>
         <div class="parametres">
                <a class="lien" href="htmlgrid3.html">
-                <div class="parametres_lien"><br><br><br>Paramètres</div></a></div>
+                <div class="parametres_lien"><br><br>Paramètres</div></a></div>
         <div class="aide">
               <a class="lien" href="help.php">
-                   <div class="aide_lien"><br><br><br>Aide</div></a></div>
+                   <div class="aide_lien"><br><br>Aide</div></a></div>
         <div class="virifocus"></div>
     </div>
     
