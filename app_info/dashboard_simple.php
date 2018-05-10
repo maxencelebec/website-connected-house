940f7d5c860d54bf46336e941f54aaa01dcf5dee
Victor Lebrun <victorhidole@gmail.com>
2018-05-10 17:15:00 +0200

    Dashboard Simple v2

--- app_info/dashboard_simple.php
+++ app_info/dashboard_simple.php
@@ -31,89 +31,30 @@
                 ?>
 
                 <div class="main">
-                    <div class="case21"></div>
-                    <div class="informations">
-                        <div class="maison_1">
-                            <div class="case2211"></div>
-                            <div class="maison_conso">
-                                <div class="maison">
 
-                                    <?php
+                    <?php
 
-                                    $req = $bdd->prepare('SELECT name,firstname,id FROM users WHERE mail=?');
-                                    $req->execute(array($_SESSION["mail"]));
-                                    while ($donnees = $req->fetch())
-                                    {
-                                        $id_user = $donnees['id'];
-                                    }
-                                    ?>
+                    $req = $bdd->prepare('SELECT id FROM users WHERE mail=?');
+                    $req->execute(array($_SESSION["mail"]));
+                    while ($donnees = $req->fetch())
+                    {
+                        $id_user = $donnees['id'];
+                    }
 
-                                    <?php
-                                    $req = $bdd->prepare('SELECT nom FROM habitation WHERE id_user= ? ');
-                                    $req->execute(array($id_user));
-
-                                    while ($donnees = $req->fetch())
-                                    {
-                                    $nom = $donnees['nom'];
-                                    echo "Maison : $nom";
-                                    }
-                                    ?>
-
-                                </div>
-                                    <div class="chartContainer"></div>
-                            </div>
-                            <div class="case2213"></div>
-                            <div class="donnees">
-                                <div class="temp_mode_secu">
-                                    <div class="temperature">
-                                        Température
-
-                                    </div>
-                                    <div class="modemaison">Mode Maison</div>
-                                    <div class="securite">Sécurité</div>
-                                </div>
-                                <div class="capteurs">
-                                    <div class="titre">Capteurs</div>
-                                    <div class="infos">
-                                        <div class="actifs">Actifs : 22</div>
-                                        <div class="inactifs">Inactifs : 3</div>
-                                        <div class="erreur">Erreur : 2</div>
-                                    </div>
-                                </div>
-                                <div class="qui_autresoptions">
-                                    <div class="quiestalamaison">Qui est à la maison ? </br></br>
-                                            <?php
-
-                                                $req = $bdd->prepare('SELECT name,firstname,id FROM users WHERE mail=?');
-                                                $req->execute(array($_SESSION["mail"]));
-                                                while ($donnees = $req->fetch())
-                                                {
-
-                                                    echo "<p style='color: #2cc872'>".$donnees["firstname"]." ".$donnees["name"]."</p>";
-                                                }
-                                            ?>
-                                    </div>
-                                    <div class="autresoptions">
-                                        <a class="lien2" href="dashboard_maison.php">
-                                            <div class="autresoptions1">Autres Options<br>...</div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="case2215"></div>
-                        </div>
-                        <div class="ajoutmaison">
-                            <div></div>
-                            <div class="boutton2">
-                                <a href="inscription_habitant_3.php" class="lien3">
-                                    <button class="ajoutermaisonlink" onclick="">+</button>
-                                </a>
-                            </div>
-                            <div></div>
-                            <div class="ajoutermaison">Ajouter Maison</div>
-                        </div>
-                    </div>
-                    <div class="case23"></div>
+                    $req = $bdd->prepare('SELECT nom FROM habitation WHERE id_user=?');
+                    $req->execute(array($id_user));
+                    while ($donnees = $req->fetch())
+                    {
+                        $nom_maison = $donnees['nom'];
+                        include_once "creation_dashboard_simple.php";
+                        creation_dashboard_simple($nom_maison);
+                    }
+                    ?>
+                    <br>
+                    <?php
+                        include "dashboard_simple_ajouter.php";
+                    ?>
+                    <br>
                 </div>
 
                 <?php
@@ -122,8 +63,6 @@
 
             </div>
 
-            <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-            <script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
 </body>
 <script src="dashboard_simple.js"></script>
 </html>
