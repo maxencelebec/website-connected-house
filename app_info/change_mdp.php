6ff26ec343b6e4c9e8a15761f50a880014752b20
ving10785 <vincent.nguyen96@ymail.com>
2018-05-11 17:04:23 +0200

    Avant TP de mort

--- app_info/change_mdp.php
+++ app_info/change_mdp.php
@@ -1,4 +1,55 @@
 ﻿<!DOCTYPE html>
+<?php
+    /* Vérification de l'URL */
+    if (ISSET($_GET['mail']) && ISSET($_GET['pass_token'])) {
+        /* Connection BDD*/
+        try
+        {
+            $bdd = new PDO('mysql:host=localapp;dbname=virifocus;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+        }
+        
+        $mail = $_GET['mail'];
+        $token = $_GET['pass_token'];
+        
+        /* Requêtes des différentes données du compte */
+        $req = $bdd->prepare('SELECT COUNT(password) FROM users WHERE mail=? AND pass_token=?');
+        $req->execute(array($mail,$token));
+        
+        while ($donnees = $req->fetch()) {
+            $compteur = $donnees['COUNT(password)'];
+        }
+        
+        if ($compteur>0) { /* Combinaison mail/pass_token correct */
+            
+            if (isset($_POST['valider']) && ($_POST['mdp']==$_POST['mdp_confirm'])) {   /* Lors de l'envoi et nouveaux mdps similaires */
+                
+                $req = $bdd->prepare('UPDATE users SET password=? WHERE mail=? AND pass_token=?');
+                $req->execute(array($mdp, $mail, $token));
+                
+                echo $mdp;
+            }
+            
+            else if (isset($_POST['valider']) && ($_POST['mdp']!=$_POST['mdp_confirm'])) { /* Lors de l'envoi et nouveaux mdps différents */
+                echo "mdp différents...";
+            }            
+        }
+        
+        else {
+            header("Location: index.php"); /* Retour à l'index car combinaison mail/pass_token incorrect */
+            exit();
+        }
+    }
+    
+    else {
+        header("Location: index.php"); /* Retour à l'index car l'URL est erronné */
+        exit();
+    }	
+	?>
+	
 <html>
 <head>
     <title>Virifocus</title>
@@ -14,7 +65,7 @@
 	
 	<div class="Main">
 
-		<form class="infos" action="?" method="POST">
+		<form class="infos" action="change_mdp.php" method="POST">
 
 			<div class="formulaire">
 				<div class="titre"><h1>Changez votre mot de passe</h1></div>
@@ -24,7 +75,7 @@
 				<input type="password" id="mdp_confirm" style="text-align: center"/>
 			</div>
 
-			<input class="valider" type="submit" value="Valider">
+			<input class="valider" type="submit" value="Valider" name="valider">
 
 			</div>
 		</form>
--- app_info/mdp_oublie.php
+++ app_info/mdp_oublie.php
@@ -1,4 +1,52 @@
 ﻿<!DOCTYPE html>
+<?php
+    /* Connection à la BDD*/
+	try
+	{
+		$bdd = new PDO('mysql:host=localapp;dbname=virifocus;charset=utf8', 'root', '');
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+	
+	/* Après le submit */
+	if (ISSET($_POST['valider'])) {
+    	/* Requêtes des différentes données du compte */
+    	$req = $bdd->prepare('SELECT COUNT(id) FROM users WHERE mail=?');
+    	$req->execute(array($_POST['mail']));
+    		
+    	/* Attribution variable des données */
+    	while ($donnees = $req->fetch()) {
+    		$compteur = $donnees['COUNT(id)'];
+    	}
+    	
+    	/* Vérification du mail dans la BDD */
+    	if ($compteur>0) {
+    	    /* Génération d'un jeton pour éviter les abus */
+    		$token = str_shuffle("0123456789azertyuiopqsdfghjklmwxcvbn");
+    		$token = substr($token,0,10);
+    		
+    		$req = $bdd->prepare('UPDATE users SET pass_token=? WHERE mail=?');
+    		$req-> execute(array($token, $_POST['mail']));
+    		?>
+    		<p class='text_envoi'>
+       		Un mail vous permettant de réinitialiser votre mot de passe vous a été envoyé.
+    		Vous le trouverez dans votre boîte de réception d'ici quelques minutes.
+    		Pensez à vérifier vos Courriers Indésirables si vous ne trouvez pas le mail.
+    		 </p>
+    		 <a href='localapp/app_info/change_mdp.php?mail=<?php echo $mail; ?>&pass_token=<?php echo $token; ?>'>  </a>
+    		 <?php
+    	}
+    	else { ?>   	
+    		<p class='text_erreur'>
+    		Nous n'avons pas reconnu votre mail. Veuillez vérifier vos identifiants.
+    		</p>
+    		<?php
+    	}
+	}
+?>
+
 <html>
 <head>
     <title>Virifocus</title>
@@ -15,7 +63,7 @@
 	</div>
 	
 	<div class="Main">
-		<form class="infos" action="mdp_oublie_post.php" method="post">
+		<form class="infos" action="mdp_oublie.php" method="post">
 			<div class="formulaire">
 				<div class="titre"><h1>Réinitialisez votre mot de passe</h1></div>
 				<div>Adresse Mail :</div>
