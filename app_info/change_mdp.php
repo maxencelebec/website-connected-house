0f2ff8f485887ba1cddd1a9587d1d58049997cb3
ving10785 <vincent.nguyen96@ymail.com>
2018-05-12 20:25:40 +0200

    Il manquait juste un echo...
    RIP

--- app_info/change_mdp.php
+++ app_info/change_mdp.php
@@ -18,12 +18,18 @@ if (ISSET($_GET['mail']) && ISSET($_GET['pass_token'])) {
     if (isset($_POST['valider'])) {     /* Après envoie */            
           
         if($_POST['mdp']===$_POST['mdp_confirm']) {
-            $mdp = sha1($_POST['mdp']);
+            $nouv_mdp = sha1($_POST['mdp']);
             $req = $bdd->prepare('UPDATE users SET password=? FROM users WHERE mail=? AND pass_token=?');
-            $req->execute(array($mdp, $mail,$token));
+            $req->execute(array($nouv_mdp,$mail,$token));
         }
         else {
-            echo "sortie 1";
+            ?>
+            
+            <p class="text_erreur">
+            	Veuillez vérifier les données saisies
+            </p>
+            
+            <?php 
         }
     }
 }
@@ -48,7 +54,7 @@ else {
 	
 	<div class="Main">
 
-		<form class="infos" action="change_mdp.php?mail=<?php $mail; ?>&pass_token=<?php $pass_token; ?>" method="POST">
+		<form class="infos" action="change_mdp.php?mail=<?php echo $_GET['mail']; ?>&pass_token=<?php echo $_GET['pass_token']; ?>" method="POST">
 
 			<div class="formulaire">
 				<div class="titre"><h1>Changez votre mot de passe</h1></div>
