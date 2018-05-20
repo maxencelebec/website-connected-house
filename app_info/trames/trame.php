1b9ffb5df047bc7ec59e88770d63c112b3887dd4
ving10785 <vincent.nguyen96@ymail.com>
2018-05-20 21:21:28 +0200

    Trames
    Récupération des logs du serveur pour le site.
    Nouvelles questions:
    1. Récupération de la bonne trame dans la BDD pour le site ?
    2. Ecriture d'une trame sur le site et envoie sur le serveur ?

---
+++ app_info/trames/trame.php
@@ -0,0 +1,97 @@
+<?php
+/* Connection à la BDD */
+try
+{
+    $bdd = new PDO('mysql:host=localapp;dbname=virifocus;charset=utf8', 'root', '');
+    echo "Connecté à la BDD";
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+    echo "ERREUR BDD ERREUR BDD ERREUR BDD";
+}
+
+/* Récupération des données sur le site fourni */
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=009D");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8" /> <link rel="stylesheet" href="#"
+    <title>traitement trame</title>
+</head>
+<body>        
+	<form method="post" action="">    
+    	<input type="submit" name="envoyer">
+    </form>
+</body>
+</html>
+
+<?php 
+/* Stockage des trames dans un array */
+$data_tab = preg_split("/([0-9]009D)/", $data, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+
+if(isset($_POST['envoyer'])) {
+    for($i=0, $size_demi=round(count($data_tab)/2); $i<$size_demi; $i++) {
+        
+        /* Récupération de la trame dans data_tab */
+        $j = 2 * $i;
+        $k = 2 * $i +1;
+        $trame = "$data_tab[$j]$data_tab[$k]";
+        echo "$trame <br />";
+        
+        /* Récupération du type de la trame */
+        $type_trame = substr($trame, 0, 1);
+        
+        /* Gestion des trames selon leur type, vérifie notamment la cohérence avec la longueur de la trame */
+        
+        /* Trame courante (33) */
+        if(($type_trame==1)&&(strlen($trame)==33)) {   
+            $num_objet = substr($trame, 1, 4);
+            $type_req = substr($trame, 5, 1);
+            $type_capteur = substr($trame, 6, 1);
+            $num_capteur = substr($trame, 7, 2);
+            $valeur = substr($trame, 9, 4);
+            $tim = substr($trame, 13, 4);
+            $checksum = substr($trame, 17, 2);
+            $timestamp = substr($trame, 19);
+            
+            echo "Décomposition: $type_trame|$num_objet|$type_req|$type_capteur|$num_capteur|$valeur|$tim|$checksum|$timestamp <br /><br />";
+            
+            /* Envoie dans la BDD */
+            $req = $bdd->prepare('INSERT INTO trame_courante(type_trame, num_objet, type_req, type_capteur, num_capteur, valeur, tim, checksum, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+            $req->execute(array($type_trame, $num_objet, $type_req, $type_capteur, $num_capteur, $valeur, $tim, $checksum, $timestamp));
+        }
+        elseif(($type_trame==1)&&(strlen($trame)!=33))  {
+            echo "Erreur longueur de la trame courante <br /><br />";
+        }
+        
+        /* Trame rapide (25) */
+        elseif(($type_trame==3)&&(strlen($trame)==25)) {
+            $num_objet = substr($trame, 1, 4);
+            $type_req = substr($trame, 5, 1);
+            $type_capteur = substr($trame, 6, 1);
+            $nbr = substr($trame, 7, 1);
+            $donnees = substr($trame, 8, 1);
+            $checksum = substr($trame, 9, 2);
+            $timestamp = substr($trame, 11);
+            
+            echo "Décomposition: $type_trame|$num_objet|$type_req|$type_capteur|$nbr|$donnees|$checksum|$timestamp <br /><br />";
+            
+            /*Envoie dans la BDD*/
+            $req = $bdd->prepare('INSERT INTO trame_rapide(type_trame, num_objet, type_req, type_capteur, nbr, donnees, checksum, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+            $req->execute(array($type_trame, $num_objet, $type_req, $type_capteur, $nbr, $donnees, $checksum, $timestamp));    
+        }
+        elseif(($type_trame==3)&&(strlen($trame)!=25)) {
+            echo "Erreur longueur de la trame rapide <br /><br />";
+        }
+    }        
+}
+?>  
