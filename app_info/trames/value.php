964fa6c38b68d8e0fbbd9e1d786084f8f470bedb
ving10785 <vincent.nguyen96@ymail.com>
2018-05-22 14:01:04 +0200

    Récupération des valeurs d'une trame
    Récupération de la dernière valeur enregistré par un capteur de la BDD. Reste à passer tout ça en refresh auto #AJAX. En modifiant un peu on peut quasiment s'en servir pour les graphes

---
+++ app_info/trames/value.php
@@ -0,0 +1,36 @@
+<?php 
+/* Connection à la BDD */
+try
+{
+    $bdd = new PDO('mysql:host=localapp;dbname=virifocus;charset=utf8', 'root', '');
+    echo "Connecté à la BDD <br /> <br />";
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+    echo "ERREUR BDD ERREUR BDD ERREUR BDD";
+}
+
+/* Requête: SELECT num_capteur, valeur WHERE plus récent PAR num_capteur */
+$req = $bdd->prepare('SELECT num_capteur, valeur FROM trame_courante AS t1 WHERE timestamp =
+                     (SELECT MAX(timestamp) FROM trame_courante AS t2 WHERE t1.num_capteur = t2.num_capteur)
+                      GROUP BY num_capteur');
+$req->execute();
+
+$req = $bdd->prepare('SELECT num_capteur, valeur, timestamp FROM trame_courante AS t1
+                         WHERE timestamp = (SELECT MAX(timestamp) FROM trame_courante AS t2 WHERE t1.num_capteur = t2.num_capteur) GROUP BY num_capteur');
+    $req->execute();
+    while($recup = $req->fetch()) {
+        $num_capteur_tab[] = $recup['num_capteur'];
+        $valeur_tab[] = $recup['valeur'];        
+        $timestamp_tab[] = $recup['timestamp'];
+    }
+    if($num_capteur_tab==null) {
+        echo "La BDD est vide!";
+    }
+    else {
+        for($i=0, $size=count($num_capteur_tab); $i<$size; $i++) {
+            echo "Capteur $num_capteur_tab[$i] de valeur $valeur_tab[$i] à $timestamp_tab[$i] <br />";
+        }
+    }
+?>
