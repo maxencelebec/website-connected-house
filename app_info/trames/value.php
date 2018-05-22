4ed8257666ace3417587ef3fa88e33150a81c7ac
ving10785 <vincent.nguyen96@ymail.com>
2018-05-22 14:04:08 +0200

    (voir commit précédent)

--- app_info/trames/value.php
+++ app_info/trames/value.php
@@ -12,25 +12,24 @@ catch(Exception $e)
 }
 
 /* Requête: SELECT num_capteur, valeur WHERE plus récent PAR num_capteur */
-$req = $bdd->prepare('SELECT num_capteur, valeur FROM trame_courante AS t1 WHERE timestamp =
-                     (SELECT MAX(timestamp) FROM trame_courante AS t2 WHERE t1.num_capteur = t2.num_capteur)
-                      GROUP BY num_capteur');
-$req->execute();
-
 $req = $bdd->prepare('SELECT num_capteur, valeur, timestamp FROM trame_courante AS t1
                          WHERE timestamp = (SELECT MAX(timestamp) FROM trame_courante AS t2 WHERE t1.num_capteur = t2.num_capteur) GROUP BY num_capteur');
-    $req->execute();
-    while($recup = $req->fetch()) {
-        $num_capteur_tab[] = $recup['num_capteur'];
-        $valeur_tab[] = $recup['valeur'];        
-        $timestamp_tab[] = $recup['timestamp'];
+$req->execute();
+    
+    
+while($recup = $req->fetch()) {
+    $num_capteur_tab[] = $recup['num_capteur'];
+    $valeur_tab[] = $recup['valeur'];        
+    $timestamp_tab[] = $recup['timestamp'];
     }
-    if($num_capteur_tab==null) {
-        echo "La BDD est vide!";
+    
+/* Vérification du contenu des tabs avant echo */
+if($num_capteur_tab==null) {
+    echo "La BDD est vide!";
+}
+else {
+    for($i=0, $size=count($num_capteur_tab); $i<$size; $i++) {
+    echo "Capteur $num_capteur_tab[$i] de valeur $valeur_tab[$i] à $timestamp_tab[$i] <br />";
     }
-    else {
-        for($i=0, $size=count($num_capteur_tab); $i<$size; $i++) {
-            echo "Capteur $num_capteur_tab[$i] de valeur $valeur_tab[$i] à $timestamp_tab[$i] <br />";
-        }
-    }
+}
 ?>
