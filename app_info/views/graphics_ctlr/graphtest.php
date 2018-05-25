5b2f09f754dd06023049084dd279e621f952c084
Victor Lebrun <victorhidole@gmail.com>
2018-05-25 16:32:31 +0200

    Animation graph

--- app_info/views/graphics_ctlr/graphtest.php
+++ app_info/views/graphics_ctlr/graphtest.php
@@ -31,6 +31,10 @@
             color: white;
         }
 
+        .ct-series-a .ct-area {
+            fill: white;
+        }
+
 
 </style>
 
@@ -97,13 +101,35 @@
 
         <script>
 
-        var data = {
+        var chart = new Chartist.Line('.ct-chart', {
 
             labels: tab_time,
             series: [tab_val],
-            showArea: true,
-        };
 
-        new Chartist.Line('.ct-chart', data);
+        }, {
+            showArea: true,
+            fullWidth: true,
+            showPoint: false,
+        });
+
+        chart.on('draw', function(data) {
+            if(data.type === 'line' || data.type === 'area') {
+                data.element.animate({
+                    d: {
+                        begin: 2000 * data.index,
+                        dur: 3000,
+                        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                        to: data.path.clone().stringify(),
+                        easing: Chartist.Svg.Easing.easeOutQuint
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
 
         </script>
