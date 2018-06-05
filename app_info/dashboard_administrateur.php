b05a8c044e9fa7c615746caed8e3e079b52b3ff3
PaulMICHEL <37437411+PaulMICHEL@users.noreply.github.com>
2018-06-05 08:37:53 +0200

    bidouille

---
+++ app_info/dashboard_administrateur.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'fetch_trame.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Virifocus</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="dashboard_maison.css" />
+    <link rel="icon" type="image/png" href="image/logo.png" />
+    <script src="jQuery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+</head>
+
+<body class="fond">
+	<div id="site">
+                
+        <?php
+        include "header.php";
+        ?>
+
+
+
+</body>
+
+</html>
